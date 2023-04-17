502a95ac645871cdc4335f3389837bcd51b897e9
avmochkina <anastasiaavmochkina@yandex.by>
2023-04-17 12:29:16 +0300

    feat: implement repearter function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const repeatTimes = options.repeatTimes || 1;
+  const separator = options.separator || '+';
+  const addition = options.addition !== undefined ? String(options.addition) : '';
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+  const additionSeparator = options.additionSeparator || '|';
+
+  let string = '';
+  for (let i = 0; i < repeatTimes; i++) {
+    string += String(str);
+
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      string += addition;
+      if (j < additionRepeatTimes - 1) {
+        string += additionSeparator;
+      }
+    }
+
+    if (i < repeatTimes - 1) {
+      string += separator;
+    }
+  }
+
+  return string;
 }
 
 module.exports = {
